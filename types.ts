1446e471eda98a1fabc51153de09fc19a9dc6dbe
Venoms20 <111142053+Venoms20@users.noreply.github.com>
2026-01-26 18:06:32 -0300

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,35 @@
+export type Page = 'PROFILE' | 'MEDS' | 'APPOINTMENTS' | 'DIARY';
+
+export interface Patient {
+  name: string;
+  age: string;
+  gender: string;
+  phone: string;
+  emergencyContact: {
+    name: string;
+    phone: string;
+  };
+}
+
+export interface Medication {
+  id: string;
+  name: string;
+  dosage: string;
+  time: string;
+  enabled: boolean;
+}
+
+export interface Appointment {
+  id: string;
+  title: string;
+  location: string;
+  date: string;
+  time: string;
+  enabled: boolean;
+}
+
+export interface DiaryEntry {
+  id: string;
+  date: string;
+  content: string;
+}
