2837e61b3a7a04fc3015a348e5f40121b97ee5a8
Venoms20 <111142053+Venoms20@users.noreply.github.com>
2026-01-26 23:59:34 -0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -38,11 +38,11 @@ export const BookOpenIcon = () => (
 );
 
 export const PlusIcon = () => (
-  <svg {...iconProps} className="w-8 h-8"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" x2="12" y2="19"></line></svg>
+  <svg {...iconProps} className="w-8 h-8"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" x2="19" y1="12" y2="12"></line></svg>
 );
 
 export const TrashIcon = () => (
-    <svg {...iconProps}><path d="M3 6h18"></path><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
+    <svg {...iconProps} className="w-5 h-5"><path d="M3 6h18"></path><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" x2="10" y1="11" y2="17"></line><line x1="14" x2="14" y1="11" y2="17"></line></svg>
 );
 
 export const BellIcon = () => (
@@ -67,4 +67,8 @@ export const SaveIcon = () => (
 
 export const EditIcon = () => (
   <svg {...iconProps} className="w-5 h-5"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+);
+
+export const SparklesIcon = () => (
+  <svg {...iconProps}><path d="M12 2L9.1 8.3L2 9.9L7.5 14.7L5.8 22L12 18.2L18.2 22L16.5 14.7L22 9.9L14.9 8.3L12 2z" /></svg>
 );
